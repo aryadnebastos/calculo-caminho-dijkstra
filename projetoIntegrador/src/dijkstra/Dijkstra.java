4a099b985f6d3a9205d0e7653322fa8d7d70eb6f
Aryadne Bastos <49688772+aryadnebastos@users.noreply.github.com>
2020-07-03 00:06:03 -0300

    finalizado

--- projetoIntegrador/src/dijkstra/Dijkstra.java
+++ projetoIntegrador/src/dijkstra/Dijkstra.java
@@ -6,38 +6,40 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class Dijkstra {
-    public void calcularRota(Vertex vertexOrigem) {
-        vertexOrigem.setDistanciaMin(0);
-        PriorityQueue<Vertex> priorityQueue = new PriorityQueue<>();
-        priorityQueue.add(vertexOrigem);
 
-        while (!priorityQueue.isEmpty()) {
-            Vertex vertex = priorityQueue.poll();
+	public void calcularRota(Vertex vertexOrigem) {
+		vertexOrigem.setDistanciaMin(0);
+		PriorityQueue<Vertex> priorityQueue = new PriorityQueue<>();
+		priorityQueue.add(vertexOrigem);
 
-            for (Edge edge : vertex.getEdges()) {
-                Vertex v = edge.getVerticeDestino();
+		while (!priorityQueue.isEmpty()) {
+			Vertex vertex = priorityQueue.poll();
+
+			for (Edge edge : vertex.getEdges()) {
+				Vertex v = edge.getVerticeDestino();
 //                Vertex u = edge.getStartVertex();
-                double peso = edge.getPeso();
-                double minDistance = vertex.getDistanciaMin() + peso;
+				double peso = edge.getPeso();
+				double minDistance = vertex.getDistanciaMin() + peso;
 
-                if (minDistance < v.getDistanciaMin()) {
-                    priorityQueue.remove(vertex);
-                    v.setVertexAnterior(vertex);
-                    v.setDistanciaMin(minDistance);
-                    priorityQueue.add(v);
-                }
-            }
-        }
-    }
+				if (minDistance < v.getDistanciaMin()) {
+					priorityQueue.remove(vertex);
+					v.setVertexAnterior(vertex);
+					v.setDistanciaMin(minDistance);
+					priorityQueue.add(v);
+				}
+			}
+		}
+	}
 
-    public List<Vertex> getShortestPathTo(Vertex vertexAlvo) {
-        List<Vertex> rota = new ArrayList<>();
+	// calcula a menor rota pro ponto alvo
+	public List<Vertex> getMenorRotaPara(Vertex vertexAlvo) {
+		List<Vertex> rota = new ArrayList<>();
 
-        for (Vertex vertex = vertexAlvo; vertex != null; vertex = vertex.getVertexAnterior()) {
-            rota.add(vertex);
-        }
+		for (Vertex vertex = vertexAlvo; vertex != null; vertex = vertex.getVertexAnterior()) {
+			rota.add(vertex);
+		}
 
-        Collections.reverse(rota);
-        return rota;
-    }
+		Collections.reverse(rota);
+		return rota;
+	}
 }
--- projetoIntegrador/src/projetoIntegrador/MainDrunks.java
+++ projetoIntegrador/src/projetoIntegrador/MainDrunks.java
@@ -1,8 +1,9 @@
 package projetoIntegrador;
 
 import java.util.ArrayList;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import javax.swing.JFileChooser;
@@ -16,6 +17,10 @@ import dados.*;
 import dijkstra.*;
 
 public class MainDrunks {
+	
+	//GRUPO: ARYADNE BASTOS, ROGÉRIO, PEDRO ARAUJO;
+	//PROJETO INTEGRADOR: ESTRUTURA DE DADOS E MATEMÁTICA DISCRETA;
+	//PROFESSORES ORIENTADORES: DANIEL OLIVEIRA, RENATA LARANJA, ELIANA CAUS;
 
 	static Scanner entrada = new Scanner(System.in);
 	static StringBuffer memoria = new StringBuffer();
@@ -27,39 +32,38 @@ public class MainDrunks {
 		int menu;
 
 		do {
-			menu = Integer.parseInt(JOptionPane.showInputDialog("Digite o que deseja fazer :"
-	                + "\n1 - Ler Arquivo txt"
-					+ "\n2 - Calcular Menor Rota"
-	                + "\n0 - Sair"));
-			
-			switch(menu){
-	        case 1 :
-	    		Path path = ReadFile();
-	    		
-	    		List<String> list = Files.readAllLines(path, StandardCharsets.UTF_8);
-	    		String[] a = list.toArray(new String[list.size()]); 
-	    		
-	    		rotasLista = InsertData(a);
-	    		JOptionPane.showMessageDialog(null, "Inserido com sucesso! Veja o resultado no Console.");
-	    		
-	        break;
-	        
-	        case 2:
-	        	
-	        	CalculatePath();
-	        	JOptionPane.showMessageDialog(null,"Rota Calculada");
-	        break;
+			menu = Integer.parseInt(JOptionPane.showInputDialog("Digite o que deseja fazer :" + "\n1 - Ler Arquivo txt"
+					+ "\n2 - Calcular Menor Rota" + "\n0 - Sair"));
 
-	        case 0:
-	        	JOptionPane.showMessageDialog(null,"FIM DA EXECUÇÃO");
-	        break;
+			switch (menu) {
+			case 1:
+				Path path = lerArquivo();
+
+				List<String> list = Files.readAllLines(path, StandardCharsets.UTF_8);
+				String[] a = list.toArray(new String[list.size()]);
+
+				rotasLista = InsertData(a);
+				JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+
+				break;
+
+			case 2:
+
+				calcularRota();
+				JOptionPane.showMessageDialog(null, "Rota Calculada");
+				break;
+
+			case 0:
+				JOptionPane.showMessageDialog(null, "FIM DA EXECUÇÃO");
+				break;
 			}
-			
-		}while(menu != 0);
-		
+
+		} while (menu != 0);
+
 	}
 
-	public static Path ReadFile() {
+	// metodo pra ler o arquivo TXT com as informações
+	public static Path lerArquivo() {
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		int result = fileChooser.showOpenDialog(null);
@@ -72,7 +76,7 @@ public class MainDrunks {
 			return arquivo.toPath();
 		return null;
 	}
-
+	//insere os dados 
 	static ArrayList<ListaSimples> InsertData(String[] arquivo) {
 		ArrayList<ListaSimples> projeto = new ArrayList<ListaSimples>();
 		try {
@@ -86,76 +90,79 @@ public class MainDrunks {
 					char pontoB = valores[1].charAt(0);
 					double distancia = Double.parseDouble(valores[2]);
 
-					AddOnList(lista, pontoA, pontoB, distancia);
+					addNaLista(lista, pontoA, pontoB, distancia);
 				}
-				System.out.println(lista.toString());
+				
 				projeto.add(lista);
 			}
-			
+			System.out.println("ARQUIVO LIDO COM SUCESSO");
 			return projeto;
-			
+
 		} catch (Exception erro3) {
 			JOptionPane.showMessageDialog(null, "Erro de gravação!", null, JOptionPane.ERROR_MESSAGE);
 		}
 		return projeto;
 	}
 
-	public static void AddOnList(ListaSimples lista, char pontoA, char pontoB, double valor) {
+	//adiciona na lista
+	public static void addNaLista(ListaSimples lista, char pontoA, char pontoB, double valor) {
 		Item informacao = new Item(pontoA, pontoB, valor);
 		lista.inserirUltimo(informacao);
 	}
-	
-	public static void CalculatePath() {
 
-        Dijkstra dijkstra = new Dijkstra();
+	public static void calcularRota() {
 
-        
-        ListaSimples[] arrayDeLista = rotasLista.toArray(new ListaSimples[rotasLista.size()]);
-        
-        Vertex v1 = null;
-        
-	    Vertex v2 = null;
+		Dijkstra dijkstra = new Dijkstra();
+
+		ListaSimples[] arrayDeLista = rotasLista.toArray(new ListaSimples[rotasLista.size()]);
+
+		// armazena as informações
+		Map<String, Vertex> map = new HashMap<String, Vertex>();
+
+		Vertex teste = map.get("A");
 
 		for (int i = 0; i < arrayDeLista.length; i++) {
-			
+
 			No atual = arrayDeLista[i].getPrim();
-			
-			int contador = 1;
-			
-		    v1 = new Vertex(atual.getInfo().getPartida() + "");
-		    
-		    char vertexAux = 0;
-		    
-		   
-			
-			while (atual != null) {
-				
-				char chegada =  atual.getInfo().getChegada();
-				
-				if (vertexAux != chegada) {
-					vertexAux = chegada;
-					
-				    v2 = new Vertex(chegada + "");
-				    
-				    v1.addAdjacente(new Edge(atual.getInfo().getQuilometro(), v1, v2));
-				} else { 
-				    v1.addAdjacente(new Edge(atual.getInfo().getQuilometro(), v1, v2));
-				}
 
-				//msg +=  contador+ "- DE: " + atual.getInfo().getPartida() + " PARA: " + atual.getInfo().getChegada() + " DISTANCIA: " + atual.getInfo().getQuilometro() + "\n"  ;
-				atual = atual.getProximo();
-				contador++;
-			}
-		
-	        dijkstra.calcularRota(v1);
-			
-			System.out.println(dijkstra.getShortestPathTo(v2));
+			map.put(atual.getInfo().getPartida() + "", new Vertex(atual.getInfo().getPartida() + ""));
 		}
+
+		// coleta informação da partida
+		for (int i = 0; i < arrayDeLista.length; i++) {
+
+			No atual = arrayDeLista[i].getPrim();
+
+			Vertex v1 = map.get(atual.getInfo().getPartida() + "");
+
+			// pra coletar a informação da chegada
+			while (atual != null) {
+
+				char chegada = atual.getInfo().getChegada();
+
+				Vertex v2 = map.get(chegada + "");
+
+				v1.addAdjacente(new Edge(atual.getInfo().getQuilometro(), v1, v2));
+
+				atual = atual.getProximo();
+
+			}
+
+		}
+		// calcula menor rota entre os pontos
+
+		dijkstra.calcularRota(map.get("A"));
+
+		char rota;
+
+		// escolhe o destino e calcula
+
+		rota = JOptionPane
+				.showInputDialog("Escolha o destino:" + "\nB" + "\nC" + "\nD" + "\nE" + "\nF" + "\nG" + "\nH").charAt(0);
+		rota = Character.toUpperCase(rota);
 		
+		JOptionPane.showMessageDialog(null,"A menor rota para este ponto é: " + dijkstra.getMenorRotaPara(map.get(rota + "")));
 
-	
 	}
-	
-
 
 }
